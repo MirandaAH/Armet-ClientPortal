843e51776443734fd66f76daa9a02cb775d0df5d
Eric Rice <eric.rice@viointech.com>
2017-08-25 19:44:16 -0700

    file names

--- controllers/upform.js
+++ controllers/upform.js
@@ -17,7 +17,7 @@ module.exports = function(app) {
         let oldPath = files.filetoupload.path; //uploaded file path
         let oldName = files.filetoupload.name; //uploaded file name
         let extension = oldName.slice((Math.max(0, oldName.lastIndexOf(".")) || Infinity) + 1); //file extension
-        let newPath = form.uploadDir + '/' + fields.client + request.user.id + Math.floor(Math.random() * 99e9) + '.' + extension; //new path --needs to be improved (potentially not unique)
+        let newPath = form.uploadDir + '/' + fields.fileName + '.' + extension; //new path --needs to be improved (potentially not unique)
         fs.rename(oldPath, newPath, function(error) {
           if (error) { console.log(error); throw error; }
           createDoc(newPath, extension, fields.client);
