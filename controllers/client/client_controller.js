46ed207705e54533ce01ccfbe20ef524ac89a9bf
Eric Rice <eric.rice@viointech.com>
2017-08-26 02:49:01 -0700

    comments

--- controllers/client/client_controller.js
+++ controllers/client/client_controller.js
@@ -4,7 +4,7 @@ let db = require('../../models');
 module.exports = function(app) {
 
 app.put('/comment/:id', isAuthenticated, function(req, res){
-  console.log(req);
+  console.log('HERE WE GO');
   db.Docs.update({
     comment: req.body.comment
   }, {
@@ -12,8 +12,10 @@ app.put('/comment/:id', isAuthenticated, function(req, res){
       id: req.params.id
     }
   }).then((data) => {
-    response.json(data);
-  });
+    res.redirect('/completeLogin');
+  }).catch((error) => {
+    console.log(error);
+  })
 });
 
 };
