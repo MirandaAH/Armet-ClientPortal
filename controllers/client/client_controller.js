8c6f8d3ba499cba395a2c0de2bd73ed7081a859e
MirandaAH <hanemiranda@gmail.com>
2017-08-26 01:16:44 -0700

    comment

--- controllers/client/client_controller.js
+++ controllers/client/client_controller.js
@@ -3,5 +3,17 @@ let db = require('../../models');
 
 module.exports = function(app) {
 
+app.put('/comment/:id', isAuthenticated, function(req, res){
+  console.log(req);
+  db.Docs.update({
+    comment: req.body.comment
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then((data) => {
+    response.json(data);
+  });
+});
 
 };
